0685e2082481aec5158dabae193ea85860a86c50
Hao <you@example.com>
2021-12-06 14:59:10 +0100

    theme setup for form filled

--- profile_sharing_app/projects/forms.py
+++ profile_sharing_app/projects/forms.py
@@ -9,17 +9,22 @@ class ProjectForm(ModelForm):
         fields = ['title', 'featured_image', 'description',
                   'demo_link', 'source_link']
         widgets = {
+            # edit field class here for display in html page 
             'tags': forms.CheckboxSelectMultiple(),
         }
 
     def __init__(self, *args, **kwargs):
+        # provide specific class name here for the field
+        # so that we can further work on css for each class
+        # widget.attrs.update is the way to force to change the attribute
         super(ProjectForm, self).__init__(*args, **kwargs)
 
         for name, field in self.fields.items():
             field.widget.attrs.update({'class': 'input'})
 
+        ### code above tries to acheive something like this:
         # self.fields['title'].widget.attrs.update(
-        #     {'class': 'input'})
+        #     {'class': 'input'})   # can also {'class': 'input', 'placeholder': 'This is default test in the box' }
 
         # self.fields['description'].widget.attrs.update(
         #     {'class': 'input'})
