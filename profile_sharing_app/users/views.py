7e57cdcd4039b4a67dd18947815425f02df86c62
Hao <you@example.com>
2021-12-07 08:46:57 +0100

    add and render for profile and projects

--- profile_sharing_app/users/views.py
+++ profile_sharing_app/users/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Profile
+
 
 def profiles(request):
     # profiles, search_query = searchProfiles(request)
@@ -6,4 +8,16 @@ def profiles(request):
     # custom_range, profiles = paginateProfiles(request, profiles, 3)
     # context = {'profiles': profiles, 'search_query': search_query,
     #            'custom_range': custom_range}
+    context = {}
     return render(request, 'users/profiles.html', context)
+
+
+def userProfile(request, pk):
+    profile = Profile.objects.get(id=pk)
+
+    topSkills = profile.skill_set.exclude(description__exact="")
+    otherSkills = profile.skill_set.filter(description="")
+
+    context = {'profile': profile, 'topSkills': topSkills,
+               "otherSkills": otherSkills}
+    return render(request, 'users/user-profile.html', context)
