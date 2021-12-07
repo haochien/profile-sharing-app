fb891a4275eb2bd99607a3f4495b9e37726e1649
Hao <you@example.com>
2021-12-07 16:52:46 +0100

    login logout set up

--- profile_sharing_app/users/views.py
+++ profile_sharing_app/users/views.py
@@ -1,5 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
 from .models import Profile
+from django.contrib.auth.models import User
+
+
+def loginUser(request):
+
+    if request.user.is_authenticated:
+        return redirect('profiles')
+
+    if request.method == 'POST':
+        username = request.POST['username'].lower()
+        password = request.POST['password']
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            print('Username does not exist')
+
+        user = authenticate(request, username=username, password=password) # this check user name & password
+        
+        if user is not None:
+            login(request, user)  #this create session and save as cookie in the browser
+            return redirect('profile')
+        else:
+            print('Username OR password is incorrect')
+
+    return render(request, 'users/login_register.html')
+
+
+
+def logoutUser(request):
+    logout(request)  #this delete user session 
+    print('User was logged out!')
+    return redirect('login')
+
+
 
 
 def profiles(request):
