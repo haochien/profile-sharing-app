45a87acc34e342ec33caae325caea8b5c2ed8c66
Hao <you@example.com>
2021-12-12 09:53:34 +0100

    nested serialization and serializer method fields

--- profile_sharing_app/api/serializers.py
+++ profile_sharing_app/api/serializers.py
@@ -3,7 +3,41 @@ from projects.models import Project, Tag, Review
 from users.models import Profile
 
 
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = '__all__'
+
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = '__all__'
+
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = '__all__'
+
+
+
 class ProjectSerializer(serializers.ModelSerializer):
+    
+    # deal with mested serializers:
+    # to fully display those FK field (not only show the id of FK field):
+    owner = ProfileSerializer(many=False)
+    tags = TagSerializer(many=True)
+
+    # method field (to get child field or any calculated results)
+    reviews = serializers.SerializerMethodField()
+    
     class Meta:
         model = Project
         fields = '__all__'
+    
+    def get_reviews(self, obj):   # method field must start with get_. obj here is the model in class, i.e. project
+        reviews = obj.review_set.all()
+        serializer = ReviewSerializer(reviews, many=True)
+        return serializer.data
